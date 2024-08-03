92bb1cfe4b90e7619914b21aae82a320e348da25
shubham9g19 <91056346+shubham9g19@users.noreply.github.com>
2024-08-03 23:45:19 +0530

    Added Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import ForgetPassword from "./pages/ForgetPassword/ForgetPassword";
+import Home from "./pages/Home/Home";
 
 function App() {
+  const { user } = useContext(Context);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/dashboard" element={user ? <Home /> : <Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
